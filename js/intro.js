eec188d9f4c2ffd62ce9af462ae5a31a10b77133
Xanooo <tamineha@yahoo.fr>
2025-02-21 22:47:25 +0100

    Refonte du site et de sa page d'accueil.

---
+++ js/intro.js
@@ -0,0 +1,8 @@
+
+
+function welcome(){
+  let title = document.getElementsByClassName("nom")[0];
+  let title2 = document.getElementsByClassName("student")[0];
+  title.textContent = "Welcome !";
+  title2.style.display = "none";
+}
