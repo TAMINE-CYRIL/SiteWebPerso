72401a92a75ba07f68c6c4e981756440bf6c7bc3
Xanooo <tamineha@yahoo.fr>
2025-03-09 14:28:24 +0100

    Modification dans le CSS et ajouts d'animations.

--- js/intro.js
+++ js/intro.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const aboutSection = document.querySelector(".about");
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          aboutSection.classList.add("show");
+        }
+      });
+    },
+    { threshold: 0.2 }
+  );
+
+  observer.observe(aboutSection);
+});
 
 
 function welcome(){
